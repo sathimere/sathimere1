61fcd31306e56d1aeb5287cc4fc6bd7331edd9ee
sathimere <gvjharda@gmail.com>
2025-12-31 21:24:00 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCNujpIBIvJXMk34yQF1VeuTVFi5M7vdc0",
   authDomain: "sathimere-415fa.firebaseapp.com",
   projectId: "sathimere-415fa",
-  storageBucket: "sathimere-415fa.firebasestorage.app",
+  storageBucket: "sathimere-415fa.appspot.com",   // ✔ FIXED
   messagingSenderId: "290492321074",
   appId: "1:290492321074:web:05aa3d9abc2d3ab7635b2b"
 };
@@ -17,7 +17,7 @@ const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
 
-// Export for all pages
+// Export globally
 window.auth = auth;
 window.db = db;
 window.storage = storage;
